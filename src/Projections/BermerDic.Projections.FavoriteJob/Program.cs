602ffe8a8cb2020d262d3e540cb12fb9387b5358
Mert Ekizler <mert.ekizler@valprovia.com>
2023-06-18 02:24:47 +0300

    Add project files.

---
+++ src/Projections/BermerDic.Projections.FavoriteJob/Program.cs
@@ -0,0 +1,10 @@
+using BermerDic.Projections.FavoriteJob;
+
+IHost host = Host.CreateDefaultBuilder(args)
+    .ConfigureServices(services =>
+    {
+        services.AddHostedService<Worker>();
+    })
+    .Build();
+
+await host.RunAsync();
